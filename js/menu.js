bb0ae3cb44b11e21bddee7d19982a61c9ea4083f
cristian andres rodriguez ortega <c.andres.r.ortega@gmail.com>
2018-07-13 17:15:51 -0500

    primer commit

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+var btnMenu = document.getElementById('btn-menu');
+var nav     = document.getElementById('nav');
+btnMenu.addEventListener('click', function () {
+
+    "use strict";
+
+    nav.classList.toggle('mostrar');
+      
+});
